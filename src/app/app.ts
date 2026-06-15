82a38690c153cea9b61de318ff635d494eb78f9b
Edwin Seclen <jtech3@dbperu.com>
2026-06-15 18:09:14 -0500

    Primera versión propuesta gestión documental 2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,45 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarouselModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('propuesta-LDM');
+
+  pantallas = [
+
+    {
+      titulo: 'Dashboard',
+      subtitulo: 'Indicadores y métricas del sistema',
+      descripcion: 'Visualización centralizada de documentos, procesos, usuarios y actividades recientes.',
+      imagen: 'assets/mockups/dashboard.png'
+    },
+
+    {
+      titulo: 'Gestión Documental',
+      subtitulo: 'Control y búsqueda de documentos',
+      descripcion: 'Consulta rápida de documentos mediante filtros avanzados, estados, procesos y versiones.',
+      imagen: 'assets/mockups/RegistroDocumentos.png'
+    },
+
+    {
+      titulo: 'Usuarios y Permisos',
+      subtitulo: 'Seguridad basada en roles',
+      descripcion: 'Administración de usuarios, roles y permisos especiales según necesidades operativas.',
+      imagen: 'assets/mockups/GestionUsuarios.png'
+    },
+
+    // {
+    //   titulo: 'Auditoría',
+    //   subtitulo: 'Trazabilidad completa',
+    //   descripcion: 'Registro detallado de accesos, consultas, modificaciones y actividades realizadas.',
+    //   imagen: 'assets/mockups/auditoria.png'
+    // }
+
+  ];
 }
